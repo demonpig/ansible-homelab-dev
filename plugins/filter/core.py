bcab43eb83db454bf3269a251aaa6b2693e28fe8
Max Mitschke <maxmitschke@fastmail.com>
2023-11-22 16:27:38 -0500

    Adding playbook to download stuff from Internet Archive

--- plugins/filter/core.py
+++ plugins/filter/core.py
@@ -9,8 +9,16 @@ def seconds_to_timestamp(a):
     except:
         raise AnsibleFilterError("Input must be an integer, provider '{}'".format(a))
 
+def create_ia_exclude_options(a):
+    exclude_string = ""
+    if isinstance(a, list):
+        for exclude in a:
+            exclude_string = exclude_string + "-e '{}'".format(exclude)
+    return exclude_string
+
 class FilterModule(object):
     def filters(self):
         return {
-            'seconds_to_timestamp': seconds_to_timestamp
+            'seconds_to_timestamp': seconds_to_timestamp,
+            'create_ia_exclude_options': create_ia_exclude_options
         }
